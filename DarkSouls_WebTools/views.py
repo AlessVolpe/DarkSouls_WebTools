0139dec26efe2c6cc61df49b8609a697e3d67254
Alessandro Volpe <37452470+AlessVolpe@users.noreply.github.com>
2024-08-20 02:41:52 +0300

    Refactored landing_page.html to index.html

--- DarkSouls_WebTools/views.py
+++ DarkSouls_WebTools/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
